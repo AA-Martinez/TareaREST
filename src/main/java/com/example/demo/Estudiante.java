0c6004d9de757af968d5affd37b6e5b81b832ef5
Alejandro Antonio Martinez Fuertes <ale_mf_99@hotmail.com>
2019-09-12 00:04:07 -0400

    Agregando Clase Estudiante, EstudianteDBA y EstudianteRepositorio

---
+++ src/main/java/com/example/demo/Estudiante.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Estudiante {
+    private @Id @GeneratedValue Long id;
+    private String nombre;
+    private int numeroCelular;
+
+    Estudiante(){}
+
+    public Estudiante(String nombre, int numeroCelular) {
+        this.nombre = nombre;
+        this.numeroCelular = numeroCelular;
+    }
+}
---
+++ src/main/java/com/example/demo/EstudianteDBA.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class EstudianteDBA {
+
+    @Bean
+    CommandLineRunner iniciarBD(EstudienteRepositorio repositorio){
+        return args -> {
+            log.info("Cargando "+ repositorio.save(new Estudiante("Alejandro Martinez",73048478)));
+            log.info("Cargando "+ repositorio.save(new Estudiante("Cristopher Castro",77743818)));
+        };
+    }
+
+}
