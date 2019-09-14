cff37fce8620ed7797ed5ebe86339a0e36bcc02d
Alejandro Antonio Martinez Fuertes <alejandro.martinez.fuertes1@gmail.com>
2019-09-14 13:44:14 -0400

    Revert "Se modifico la clase estudiante separando nombre y apellido, se creo EstudianteResourceAssambler para implementar assambler a la hora de hacer POST, se modifico en EstudianteControlador los PostMapping para implementar assambler, y en EstudianteDBA se agrego apellido a los datos existentes"
    This reverts commit cade8e2ae70384fe1851ba70326a155d9b01f476.

--- src/main/java/com/example/demo/Estudiante.java
+++ src/main/java/com/example/demo/Estudiante.java
@@ -11,25 +11,12 @@ import javax.persistence.Id;
 class Estudiante {
     private @Id @GeneratedValue Long id;
     private String nombre;
-    private String apellido;
     private int numeroCelular;
 
     Estudiante(){}
 
-    public Estudiante(String nombre, String apellido, int numeroCelular) {
+    public Estudiante(String nombre, int numeroCelular) {
         this.nombre = nombre;
-        this.apellido = apellido;
         this.numeroCelular = numeroCelular;
     }
-
-    public String obtenerNombre(){
-        return this.nombre + " " + this.apellido;
-    }
-
-    public void agregarNombre(String nombre){
-        String[] partes = nombre.split(" ");
-        this.nombre = partes[0];
-        this.apellido = partes[1];
-    }
-
 }
